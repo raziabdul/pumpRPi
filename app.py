34dcdb84ff17bd9dff424675cd1bbc1743fd2d2b
Faizal215 <127084108+Faizal215@users.noreply.github.com>
2023-06-21 14:18:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def predict():
         # Validate input
         if not validate_input(input_data):
             error_msg = "Invalid input. Please provide all required fields."
-            return render_template('home.html', error_msg=error_msg, **request.form)
+            return render_template('home.html', error_msg=error_msg)
 
         # Create a DataFrame from the input dictionary
         input_df = pd.DataFrame([input_data])
@@ -34,7 +34,7 @@ def predict():
         probability = model.predict_proba(input_df)[:, 1]
 
         # Determine the pump condition and confidence
-        pump_conditions = ["healthy","inception","developing","mild","severe","unstable"]
+        pump_conditions = ["healthy", "inception", "developing", "mild", "severe", "unstable"]
         condition = pump_conditions[prediction[0]]
         confidence = probability[0] * 100
 
@@ -42,11 +42,18 @@ def predict():
         output1 = f"The pump is in {condition} condition"
         output2 = f"Confidence: {confidence:.2f}%"
 
-        return render_template('home.html', output1=output1, output2=output2, **request.form)
+        return render_template('home.html', output1=output1, output2=output2)
     except Exception as e:
         error_msg = f"Error: {str(e)}"
-        return render_template('home.html', error_msg=error_msg, **request.form)
+        return render_template('home.html', error_msg=error_msg)
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 def validate_input(input_data):
     # Check if all required fields are present
@@ -58,7 +65,6 @@ def validate_input(input_data):
         if field not in input_data:
             return False
     return True
-
 
 if __name__ == "__main__":
     app.run()
